7484173d539c49718d443a00c27235ebfb272540
hanifudin0597 <hanifudin0597@gmail.com>
2022-03-10 20:36:47 +0700

    update tugas 3

--- tugas3.js
+++ tugas3.js
@@ -33,4 +33,5 @@ const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => {
 console.log(seleksiNilai(7, 20 , [2, 25, 4, 14, 17, 30, 8]))
 console.log(seleksiNilai(15, 3 , [2, 25, 4, 14, 17, 30, 8]))
 console.log(seleksiNilai(5, 17 , [2, 25, 4]))
+console.log(seleksiNilai(5, 17 , [2, 25, 4, 1, 30, 18]))
 console.log(seleksiNilai(5, 17 , [2, 25, 4, 1, 30, "18"]))
